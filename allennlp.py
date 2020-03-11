f6eb012f4329a5711be9b6a8b045f5d3cbf422f8
jsrozner <jsrozner@gmail.com>
2020-03-11 12:34:27 -0700

    predownload of other branch

---
+++ allennlp.py
@@ -0,0 +1,9 @@
+from allennlp.models.archival import load_archive
+from allennlp.service.predictors import Predictor
+
+archive = load_archive(
+            "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo-constituency-parser-2018.03.14.tar.gz"
+        )
+predictor = Predictor.from_archive(archive, 'constituency-parser')
+
+predictor.predict_json({"sentence": "This is a sentence to be predicted!"})
