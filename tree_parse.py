8405f8ab6a10c3236005a89d448555f602a97019
jsrozner <jsrozner@gmail.com>
2020-03-11 13:12:24 -0700

    add tree_parse.py

--- tree_parse.py
+++ tree_parse.py
@@ -1,9 +1,7 @@
 from allennlp.models.archival import load_archive
-from allennlp.service.predictors import Predictor
+from allennlp.predictors import Predictor
 
-archive = load_archive(
-            "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo-constituency-parser-2018.03.14.tar.gz"
-        )
+archive = load_archive("./rep_allennlp/elmo-constituency-parser-2018.03.14.tar.gz" )
 predictor = Predictor.from_archive(archive, 'constituency-parser')
 
 predictor.predict_json({"sentence": "This is a sentence to be predicted!"})
