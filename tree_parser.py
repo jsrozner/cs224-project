2b3eb9db75ef76cd4a15b8153d00a0b6e036908a
Andra Fehmiu <andrafehmiu@Andras-MacBook-Pro.local>
2020-03-12 17:00:59 -0700

    new parser

--- tree_parser.py
+++ tree_parser.py
@@ -11,9 +11,9 @@ def getAllenTree(sentence):
     output = predictor.predict_json(sentence)
     tree = Tree.fromstring(output["trees"])
 
-    for subtree in tree:
-        if type(subtree) == Tree:
-            print("Tree:", subtree)
+    # for subtree in tree:
+    #     if type(subtree) == Tree:
+    #         print("Tree:", subtree)
 
     return tree
 
@@ -45,7 +45,89 @@ def extractPhraseFiltered(sentence, label1="NP", label2= "NNP"):
     for st in tree.subtrees(filter=lambda x: x.label() == label1 and x[0].label == label2):
         print(st)
 
+
+def find_VP(tree):
+    """Recurse on a constituency parse tree until we find verb phrases"""
+
+    # Recursion is annoying because we need to check whether each is a list or not
+    def recurse_on_children():
+        assert 'children' in tree
+        result = []
+        for child in tree['children']:
+            res = find_VP(child)
+            if isinstance(res, tuple):
+                result.append(res)
+            else:
+                result.extend(res)
+        return result
+
+    for i in tree.subtrees:
+        if i(filter=lambda x: x.node == "VP"):
+            return [(tree["word"], "VP")]
+
+    # if 'VP' in tree['attributes']:
+    #     # # Now we'll get greedy and see if we can find something better
+    #     # if 'children' in tree and len(tree['children']) > 1:
+    #     #     recurse_result = _recurse_on_children()
+    #     #     if all([x[1] in ('VP', 'NP', 'CC') for x in recurse_result]):
+    #     #         return recurse_result
+    #     return [(tree['word'], 'VP')]
+
+    # base cases
+
+    if 'NP' in tree['attributes']:
+        return [(tree['word'], 'NP')]
+    # No children
+    if not 'children' in tree:
+        return [(tree['word'], tree['attributes'][0])]
+
+    # If a node only has 1 child then we'll have to stick with that
+    if len(tree['children']) == 1:
+        return recurse_on_children()
+    # try recursing on everything
+    return recurse_on_children()
+
+def traverse(t):
+
+    newtree = ParentedTree.convert(t)
+    try:
+        newtree.label()
+    except AttributeError:
+        return
+    else:
+        print("height", newtree.height())
+        if newtree.height() == 2:   #child nodes
+            print("here!!!", newtree)
+            return
+
+        else:
+            print("no height")
+
+        for child in newtree:
+            traverse(child)
+
+def getHeightNode(tree):
+    newtree = ParentedTree.convert(tree)
+    tree_dict = {}
+    for i in newtree.subtrees():
+        tree_dict = {}
+        print(i.height(), i)
+        leaf_index = leaf_values.index('nice')
+        tree_location = newtree.leaf_treeposition(leaf_index)
+
+def getTreeLeaves(tree):
+    leafpos = [tree.leaf_treeposition(n) for n, x in enumerate(tree.leaves())]
+    level1_subtrees = [tree[path[:-1]] for path in leafpos]
+    print(leafpos)
+    for x in level1_subtrees:
+        print(x, end=" ")
+
+
 if __name__ == "__main__":
     sentence = {"sentence": "This is a sentence to be predicted!"}
     tree = getAllenTree(sentence)
-    print(traverseAllenTree(tree))
+    # print(traverseAllenTree(tree))
+    # print(find_VP(tree))
+    # print(traverse(tree))
+    # print(getHeightNode(tree))
+    print(getTreeLeaves(tree))
