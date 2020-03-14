7238d881497bf45d66076636d518798eb3a142dc
jsrozner <jsrozner@gmail.com>
2020-03-14 00:35:10 -0700

    tree parser updated

--- tree_parser.py
+++ tree_parser.py
@@ -1,142 +1,156 @@
 from allennlp.models.archival import load_archive
 from allennlp.predictors import Predictor
-from nltk import Tree
+from nltk import Tree, sent_tokenize
 from pprint import pprint as pp
-from nltk.tree import ParentedTree
+from typing import Dict, NoReturn, List, Dict, Tuple
 
-
-def getAllenTree(sentence):
+# utils
+def load_allen_predictor():
     archive = load_archive("./rep_allennlp/elmo-constituency-parser-2018.03.14.tar.gz")
     predictor = Predictor.from_archive(archive, 'constituency-parser')
+    return predictor
+
+def get_allen_tree(sentence) -> Tree:
     output = predictor.predict_json(sentence)
-    tree = Tree.fromstring(output["trees"])
-
-    # for subtree in tree:
-    #     if type(subtree) == Tree:
-    #         print("Tree:", subtree)
-
-    return tree
-
-def traverseAllenTree(tree):
-    np_trees = []
-    try:
-        tree.label()
-    except AttributeError:
-        return
-
-    newtree = ParentedTree.convert(tree)
-    if newtree.label() == "VP" or newtree.label() == "NP":
-        current = newtree
-        while current.parent() is not None:
-            while current.left_sibling() is not None:
-                 if current.left_sibling().label() == "NP" or current.left_sibling().label() == "VP":
-                     np_trees.append(current.left_sibling())
-                 current = current.left_sibling()
-            current = current.parent()
-
-    for child in tree:
-        traverseAllenTree(child)
-    return np_trees
+    pp(output["trees"])
+    return Tree.fromstring(output["trees"])
 
 
-def extractPhraseFiltered(sentence, label1="NP", label2= "NNP"):
+# parse the tree for a question into the phrasal replacement candidates
+# Note that tree is **modified** (all leaves will have an appended "_index"
+def _question_to_phrases_for_replacement(start_tree: Tree) -> List[Dict]:
+    """
+    Parses a tree into the set of phrases we consider for paraphrasing.
 
-    tree = getAllenTree(sentence)
-    for st in tree.subtrees(filter=lambda x: x.label() == label1 and x[0].label == label2):
-        print(st)
+    :param tree: The input tree representing a sentence. Parse looking for phrases of certain type
+    :return: List (indexed by label type) => tuple of (words in phrase, len of phrase)
+    """
+
+    # Modifies a tree ** be careful **
+    def _add_indices_to_terminals(tree):
+        for idx, _ in enumerate(tree.leaves()):
+            tree_location = tree.leaf_treeposition(idx)
+            non_terminal = tree[tree_location[:-1]]
+            non_terminal[0] = non_terminal[0] + "_" + str(idx)
+        return tree
+
+    def _internal_traverse(tree):
+        for child in tree:
+            if not isinstance(child, Tree): # early fail if it's not a tree
+                return
+
+            label = child.label()
+            num_leaves = len(child.leaves())
+            if num_leaves > stop_leaves or label not in tree_label_to_index:            # haven't met stop criterion, so recurse
+                _internal_traverse(child)
+            else:  # valid stop (few enough leaves, and it's in the replacement list)
+                phrase = child.leaves()
+                nonlocal phrases_to_paraphrase
+                phrases_to_paraphrase += [(phrase, tree_label_to_index[label])]
+
+    tree_with_leaf_indices = _add_indices_to_terminals(start_tree)   # add indices to tree roots
+    phrases_to_paraphrase = []                   # accumulation set from tree traversal
+    _internal_traverse(tree_with_leaf_indices)   # modifies phrases_to_paraphrase
+
+    output_list = []                                # final accumulation (postprocess)
+    for p, label_numeric in phrases_to_paraphrase:  # each p is a tuple (phrase, label_numeric)
+        phrase_start_idx = p[0].split("_")[1]
+        for i, word in enumerate(p):                # iterate over the words in each phrase, p
+            p[i] = word.split("_")[0]
+        output_list += [{"phrase": p,
+                         "phrase_type": label_numeric,
+                         "phrase_start_idx": int(phrase_start_idx)}]
+
+    return output_list
 
 
-def find_VP(tree):
-    """Recurse on a constituency parse tree until we find verb phrases"""
+def _question_to_nonreplacement_portions(replacement_candidates, sentence):
+    non_replacement_list = []
+    last_unused = 0
+    for p in replacement_candidates:
+        phrase = p["phrase"]
+        phrase_len = len(phrase)
+        phrase_start = p["phrase_start_idx"]
+        if phrase_start > last_unused:
+            non_replacement_list += [{"phrase": sentence[last_unused:phrase_start],
+                                      "phrase_start_idx": last_unused}]
+        last_unused = phrase_start + phrase_len
 
-    # Recursion is annoying because we need to check whether each is a list or not
-    def recurse_on_children():
-        assert 'children' in tree
-        result = []
-        for child in tree['children']:
-            res = find_VP(child)
-            if isinstance(res, tuple):
-                result.append(res)
-            else:
-                result.extend(res)
-        return result
+    return non_replacement_list
 
-    for i in tree.subtrees:
-        if i(filter=lambda x: x.node == "VP"):
-            return [(tree["word"], "VP")]
 
-    # if 'VP' in tree['attributes']:
-    #     # # Now we'll get greedy and see if we can find something better
-    #     # if 'children' in tree and len(tree['children']) > 1:
-    #     #     recurse_result = _recurse_on_children()
-    #     #     if all([x[1] in ('VP', 'NP', 'CC') for x in recurse_result]):
-    #     #         return recurse_result
-    #     return [(tree['word'], 'VP')]
+def parse_question_tree_for_phrases(tree : Tree) -> Tuple:
+    orig_sentence = tree.leaves()       # must do before calling the
+    replacement_candidates = _question_to_phrases_for_replacement(tree)
+    non_replacement = _question_to_nonreplacement_portions(replacement_candidates, orig_sentence)   # orig sentence must be generated prior to calling replacementcandidates
 
-    # base cases
+    return replacement_candidates, non_replacement
 
-    if 'NP' in tree['attributes']:
-        return [(tree['word'], 'NP')]
-    # No children
-    if not 'children' in tree:
-        return [(tree['word'], tree['attributes'][0])]
 
-    # If a node only has 1 child then we'll have to stick with that
-    if len(tree['children']) == 1:
-        return recurse_on_children()
-    # try recursing on everything
-    return recurse_on_children()
+def context_to_replacement_phrase_sets(context_sentences : List[str]) -> List[List[str]]:
+    """
+    :param context_sentences: a list of strings (each string a sentence)
+    :return: List (indexed by type of phrase) of List of phrases (type of phrase, int => list of phrases)
+    """
 
-def traverse(t):
+    def _internal_traverse(tree):
+        for child in tree:
+            if not isinstance(child, Tree): # early fail if it's not a tree
+                return
 
-    newtree = ParentedTree.convert(t)
-    try:
-        newtree.label()
-    except AttributeError:
-        return
-    else:
-        print("height", newtree.height())
-        if newtree.height() == 2:   #child nodes
-            print("here!!!", newtree)
-            return
+            label = child.label()
+            num_leaves = len(child.leaves())
+            if num_leaves > stop_leaves or label not in tree_label_to_index:            # haven't met stop criterion, so recurse
+                _internal_traverse(child)
+            else:  # valid stop (few enough leaves, and it's in the replacement list)
+                phrase = child.leaves()
+                phrase_joined = " ".join(phrase)
+                phrase_type_to_phrases[tree_label_to_index[label]][phrase_joined] = phrase  # only save each phrase once
 
-        else:
-            print("no height")
 
-        for child in newtree:
-            traverse(child)
+    # Use dictionaries here to avoid multiple copies of the same phrase. Then throw away the key since not needed
+    phrase_type_to_phrases = [dict() for i in range(num_replacement_node_types)]
 
-def getHeightNode(tree):
-    newtree = ParentedTree.convert(tree)
-    tree_dict = {}
-    for i in newtree.subtrees():
-        tree_dict = {}
-        print(i.height(), i)
-        leaf_index = leaf_values.index('nice')
-        tree_location = newtree.leaf_treeposition(leaf_index)
+    # Iterate over each sentence tree in the context
+    for idx, s in enumerate(context_sentences):
+        context_sentences[idx] = get_allen_tree({"sentence": s})
+    for t in context_sentences:     # they are trees now
+        _internal_traverse(t)
 
-def getNodeLeaves(tree):
-    # leafpos = [tree.leaf_treeposition(n) for n, x in enumerate(tree.leaves())]
-    # level1_subtrees = [tree[path[:-1]] for path in leafpos]
-    # print(leafpos)
-    # for x in level1_subtrees:
-    #     print(x, end=" ")
-    tree_dict = {}
-    for i in tree.subtrees():
-        if i.label() not in tree_dict:
-            tree_dict[i.label()] = [(i.leaves(),len(i.leaves()))]
-        else:
-            tree_dict[i.label()] += [(i.leaves(),len(i.leaves()))]
-
-    return tree_dict
+    return [list(x.values()) for x in phrase_type_to_phrases]      # throw away the keys and keep values (the separated phrases)
 
 
 if __name__ == "__main__":
-    sentence = {"sentence": "This is a sentence to be predicted!"}
-    tree = getAllenTree(sentence)
-    # print(traverseAllenTree(tree))
-    # print(find_VP(tree))
-    # print(traverse(tree))
-    # print(getHeightNode(tree))
-    print(getNodeLeaves(tree))
-    # print(getNodePosition(tree))
+    # tree and config setup
+    stop_leaves = 3
+    replacement_node_types = ["NP", "VP", "VBZ", "VBN"] # todo
+
+    # pre-run setup
+    num_replacement_node_types = len(replacement_node_types)
+    tree_label_to_index = {name: i for i, name in enumerate(replacement_node_types)}
+
+    # allen setup
+    predictor = load_allen_predictor()
+
+    # sentence setup
+    sentence = {"sentence": "This is a sentence to be predicted from a sentence"}
+    tree = get_allen_tree(sentence)
+    pp(tree)
+
+    # context setup
+    context_paragraph = "This is a context paragraph about coding in python. Python is great. It has really cool typing support."
+    context_sentences = sent_tokenize(context_paragraph)
+
+    # example of direct string parse to save compute time on allen predictor
+    # tree_string = ('(S (NP (DT This)) (VP (VBZ is) (NP (NP (DT a) (NN sentence)) (SBAR (S (VP (TO to) (VP (VB be) (VP (VBN predicted)))))))) (. !))')
+    # tree = Tree.fromstring(tree_string)
+    # pp(tree_string)
+
+
+    # Actual run - parse question and context
+    replace, nonreplace = parse_question_tree_for_phrases(tree)
+
+    pp(replace)
+    pp(nonreplace)
+    pp(context_to_replacement_phrase_sets(context_sentences))
+
